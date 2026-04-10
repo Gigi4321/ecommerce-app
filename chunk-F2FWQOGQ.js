2524711a6a99908a9b683e3cbdfaa6ac380c25fa
gigi4321 <engyosman702@gmail.com>
2026-04-10 14:32:42 +0200

    Auto-generated commit

---
+++ chunk-F2FWQOGQ.js
@@ -0,0 +1,1 @@
+var t={url:"http://localhost:4200/",baseUrl:"https://ecommerce.routemisr.com"};export{t as a};
---
+++ chunk-SA5FXEG6.js
@@ -0,0 +1,1 @@
+import{Jb as m,Qa as t,lb as e,mb as a}from"./chunk-MJLM5QAF.js";import"./chunk-C6Q5SG76.js";var p=class o{static \u0275fac=function(n){return new(n||o)};static \u0275cmp=t({type:o,selectors:[["app-not-found"]],decls:2,vars:0,template:function(n,s){n&1&&(e(0,"p"),m(1,"not-found works!"),a())},encapsulation:2})};export{p as NotFoundComponent};
